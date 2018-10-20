b9ca25da4fac52e75322c537aaade8aa73077dcc
Sofie Ohlsson <anna.sofie.ohlsson@gmail.com>
2018-10-20 11:09:41 +0200

    Version utan substring

---
+++ Uppgift-1.js
@@ -0,0 +1,15 @@
+function Person (namn, epost, mobil, gatuadress, postnummer, ort){
+    this.namn = namn; 
+    this.epost = epost; 
+    this.mobil = mobil;
+    let adress = {};
+    adress.gatuadress = gatuadress; 
+    adress.postnnummer = postnummer; 
+    adress.ort = ort;  
+    this.print = function(){
+       console.log(this.namn + "\n" + "\n" + adress.gatuadress + "\n" + adress.postnnummer + " " + adress.ort); 
+    };
+}
+
+let sofie = new Person("Sofie Ohlsson", "anna.sofie.ohlsson@gmail.com", "0745987245", "Stråkvägen 1", "169 35", "Solna"); 
+sofie.print(); 
